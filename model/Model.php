e31461faaae944be7290d09e291f668b29626a1c
libsss01 <libsss167@gmail.com>
2026-06-09 09:56:41 +0000

    Model

--- model/Model.php
+++ model/Model.php
@@ -7,15 +7,10 @@ function getConnexion()
     $password = "";
     $dbname = "senlogisbd";
 
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-    $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ];
+    $sql = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
     try {
-        return new PDO($dsn, $user, $password, $options);
+        return new PDO($sql, $user, $password);
     } catch (PDOException $error) {
         die("Erreur connexion BD: " . $error->getMessage());
     }
